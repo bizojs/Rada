4e4a8616152ca344e29bf138b15236d0817c7686
Iskawo <bradwrightdiscord@gmail.com>
2020-11-14 16:18:41 +0000

    stats and upload commands

---
+++ src/commands/Misc/Stats.js
@@ -0,0 +1,32 @@
+const { Command } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js');
+const os = require('os');
+
+class StatsCommand extends Command {
+    constructor() {
+        super('stats', {
+           aliases: ['stats'],
+           category: 'Miscellaneous',
+           description: 'Statistics of the bot.'
+        });
+    }
+
+    async exec(message) {
+      const embed = new MessageEmbed()
+        .setTitle('Statistics and information')
+        .setColor(this.client.color)
+        .setFooter(message.author.username)
+        .setTimestamp()
+        .setThumbnail(this.client.user.avatarURL({format:'png'}))
+        .setDescription(`Detailed information about ${this.client.user.username}'s hardware and other statistics`)
+        .addField('Operating system', `${process.platform === 'linux' ? 'Ubuntu 18.04' : 'Windows 10'} ${process.arch}`)
+        .addField('CPU', os.cpus()[0].model)
+        .addField('Memory usage', `\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) > 1024 ? `${(process.memoryUsage().heapUsed / 1024 / 1024 / 1024).toFixed(2)} GB` : `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`}\` used\n\`${(os.totalmem() / 1024 / 1024).toFixed(2) > 1024 ? `${(os.totalmem() / 1024 / 1024 / 1024).toFixed(2)} GB` : `${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`}\` available`, true)
+        .addField('CPU usage', `${(process.cpuUsage().system / 1024 / 1024).toFixed(2)}%`, true)
+        .addField('Users', `\`${this.client.guilds.cache.reduce((a, c) => a + c.memberCount, 0).toLocaleString()}\``)
+        .addField('Guilds', `\`${(this.client.guilds.cache.size).toLocaleString()}\` guild${this.client.guilds.cache.size === 1 ? '' : 's'} (**${(this.client.channels.cache.size).toLocaleString()}** total channels)`)
+      return message.channel.send(embed);
+    }
+}
+
+module.exports = StatsCommand;
---
+++ src/commands/Utility/Upload.js
@@ -0,0 +1,44 @@
+const { Command } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js');
+const centra = require('@aero/centra');
+
+
+class UploadCommand extends Command {
+    constructor() {
+        super('upload', {
+           aliases: ['upload'],
+           category: 'Utility',
+           description: 'Upload an image to Imgur.',
+           cooldown: 60000,
+           ratelimit: 1
+        });
+    }
+
+    async exec(message) {
+      if (message.attachments.first() === undefined) {
+        return message.channel.send('**Image attachment/file is missing**');
+      }
+      try {
+        const res = await centra('https://api.imgur.com/3', 'POST')
+          .path('/image')
+          .header('Authorization', `Client-ID ${process.env.API_IMGUR_CLIENT_ID}`)
+          .body({ image: message.attachments.first().url })
+          .send()
+          .then(res => res.json);
+        await message.author.send(`Here is the link to delete the image if needed: <https://imgur.com/delete/${res.data.deletehash}>`)
+          .then(() => {})
+          .catch(err => {
+            this.client.users.cache.get(this.client.ownerID[0]).send({ embed: new MessageEmbed()
+              .setColor(this.client.color)
+              .addField('Upload command', `**${message.author.tag}** has just uploaded an image, but i was unable to dm them.\nTheir delete URL is: ${imgur_delete + res.data.deletehash}\nThe image they uploaded:`)
+              .setImage(res.data.link)
+            })
+          });
+        return message.channel.send('Here is the uploaded image link: ' + res.data.link);
+      } catch {
+        return message.channel.send('**Image attachment/file is missing**');
+      }
+    }
+}
+
+module.exports = UploadCommand;
