86f8a25fbcca514ac72a9af1e51ef164afe0dd5e
Iskawo <bradwrightdiscord@gmail.com>
2020-12-16 10:51:44 +0000

    smol updates + fixes

--- src/commands/Misc/Jumbo.js
+++ src/commands/Misc/Jumbo.js
@@ -19,23 +19,26 @@ class JumboCommand extends Command {
     }
 
     async exec(message, args) {
-      let emote = args.emote;     
-      try {
-          let emoji = this.client.emojis.cache.get(emote.split(':').pop().replace(/>/g, ''));
-          return message.util.send('', {
-              files: [emoji.url]
-          });
-      } catch (e) {
-          let id = emote.split(/:+/g).pop().replace(/>+/g, '');
-          let extension = emote.startsWith('<a:') ? '.gif' : '.png';
-          const res = await req(`https://cdn.discordapp.com/emojis/${id}${emote.startsWith('<a:') ? '.gif' : '.png'}?v=1`, 'GET').send()
-          if (res.statusCode === 404) {
-              return message.responder.error('**That is not a valid emote**');
-          }
-          return message.util.send('', {
-            files: [`https://cdn.discordapp.com/emojis/${id}${extension}?v=1`]
-          });
-      }
+        if (!args.emote) {
+            return message.responder.error('**Please provide an emote**');
+        }
+        let emote = args.emote;     
+        try {
+            let emoji = this.client.emojis.cache.get(emote.split(':').pop().replace(/>/g, ''));
+            return message.util.send('', {
+                files: [emoji.url]
+            });
+        } catch (e) {
+            let id = emote.split(/:+/g).pop().replace(/>+/g, '');
+            let extension = emote.startsWith('<a:') ? '.gif' : '.png';
+            const res = await req(`https://cdn.discordapp.com/emojis/${id}${emote.startsWith('<a:') ? '.gif' : '.png'}?v=1`, 'GET').send()
+            if (res.statusCode === 404) {
+                return message.responder.error('**That is not a valid emote**');
+            }
+            return message.util.send('', {
+                files: [`https://cdn.discordapp.com/emojis/${id}${extension}?v=1`]
+            });
+        }
     }
 }
 
