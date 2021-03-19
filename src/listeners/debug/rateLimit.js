1e484ad50478c288f187f16527656dc4600e0ddf
Iskawo <bradwrightdiscord@gmail.com>
2021-03-19 16:36:40 +0000

    Ignore spam warning for reactions (pagination)

--- src/listeners/debug/rateLimit.js
+++ src/listeners/debug/rateLimit.js
@@ -9,18 +9,24 @@ module.exports = class RateLimit extends Listener {
     }
 
     async exec(rateLimitInfo) {
+        let parts = rateLimitInfo.path.split(/\//);
+        if (parts[5] && parts[5] === 'reactions') return;
+
         this.client.log.rateLimit(rateLimitInfo, this.client);
-        let guild = this.client.channels.cache.get(rateLimitInfo.path.split(/\//)[2]).guild;
+
+        let guild = this.client.channels.cache.get(parts[2]).guild;
         let caching = await this.client.users.fetch(guild.ownerID);
         let guildOwner = this.client.users.cache.get(caching.id);
+
         let WarnEmote = this.client.emojis.cache.get(guild.emoteID.info);
+
         let embed = this.client.util.embed()
             .setColor(this.client.color)
             .setAuthor('[Spam Warning]', WarnEmote.url)
             .setThumbnail(this.client.avatar)
             .setDescription(`**${this.client.user.username}** is currently being ratelimited`)
             .addField('Timeout', rateLimitInfo.timeout, true)
-            .addField('Method', `${rateLimitInfo.method} ➜ ${rateLimitInfo.path.split(/\//)[3]}`, true)
+            .addField('Method', `${rateLimitInfo.method} ➜ ${parts[3]}`, true)
             .addField('Path', `\`${rateLimitInfo.path}\``)
             .addField('Guild', `${guild.name}\n${guild.id}`, true)
             .addField('Guild Owner', `${guildOwner.tag}\n${guild.ownerID}`, true)
