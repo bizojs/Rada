9a7155540a795bdb96255095b683fa141c7be811
Brys <35676267+brys0@users.noreply.github.com>
2021-08-09 22:44:57 -0500

    Recreate example.config.js

---
+++ src/example.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  	prefix: '',
+  	devPrefix: '',
+  	production: true, // don't change this. if you want to change the default prefix, edit the "prefix" section
+  	owners: ['YOUR_ID'],
+  	mongooseUrl: 'mongodb://localhost/DB_NAME',
+    ApiPort: 8000
+};
